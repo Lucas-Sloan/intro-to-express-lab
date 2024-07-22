26768b21dd7cf10aa1c67bccf4fd56c4dc265d07
Lucas-Sloan <Lucasg.sloan@gmail.com>
2024-07-22 13:11:54 -0500

    Added Greet the User functionality

--- server.js
+++ server.js
@@ -2,3 +2,24 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.get('/greetings/:username', (req, res) => {
+    const username = req.params.username;
+    const greetings = [
+        `Hello there, ${username}!`,
+        `What a delight it is to see you once more, ${username}`,
+        `Salutations, ${username}!`
+    ];
+
+    const randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
+
+    res.send(randomGreeting);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
